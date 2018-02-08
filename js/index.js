14982f347776d0b1fd21dae7f2f8551388c927e8
lidiecitass <lid_26_90@hotmail.com>
2018-02-08 09:21:08 -0500

    Logeo con Google

--- js/index.js
+++ js/index.js
@@ -1,7 +1,35 @@
-window.addEventListener('load', function() { 
+window.addEventListener('load', function () {
+  let validateEmail = false;
+  let validatePassword = false;
   let btnLogin = document.getElementById('btnLogin');
-  btnLogin.addEventListener('click', function(event) {
+  let btnGoogle = document.getElementById('btnGoogle');
+  btnLogin.addEventListener('click', function (event) {
     event.preventDefault();
     window.location.href = 'views/instaCollage.html';
   });
+  btnGoogle.addEventListener('click', function (event) {
+    event.preventDefault();
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+      let token = result.credential.accessToken;
+      let user = result.user;
+      
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          alert("Bienvenido a Insta Collage");
+          window.location.href = 'views/instaCollage.html';
+        } else {
+          console.log("NO Esta logeado");
+        }
+      });
+    }).catch(function (error) {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      let email = error.email;
+      let credential = error.credential;
+      // ...
+    });
+  });
+
+
 });
